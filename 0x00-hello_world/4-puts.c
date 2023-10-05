508c931c40344e3443249671db1c9381f2ce05cc
kingcoder91 <kingkwofie@gmail.com>
2023-10-05 15:22:05 -0700

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - this is the main function
+ * main	- this is the main function,
  *
- * Return:always0
+ * Return: always 0
  */
-
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
-     	return (0);
+	return (0);
 }
